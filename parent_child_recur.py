ec0fefe66a559c4a2cbda51f72d192ce2919ca0c
KasatkinVl <KasatkinVl@ya.ru>
2020-05-18 18:41:17 +0300

    Upfate

---
+++ parent_child_recur.py
@@ -0,0 +1,24 @@
+classes = {}
+
+n = int(input())
+for i in range(n):
+    data = input().split(' : ')
+
+    if len(data) == 1:
+        data.append("")
+
+    classes[data[0]] = data[1].split()
+
+print(classes)
+
+def is_parent(child, parent):
+    if parent in classes[child] or child == parent:
+        return True
+
+    if len(classes[child]):
+        return any(map(lambda x: is_parent(x, parent), classes[child]))
+
+for _ in range(int(input())):
+    data = input().split()
+    print("Yes" if is_parent(data[1], data[0]) else "No")
+
