4011264532360d547e552d9851aa740382c22979
KasatkinVl <KasatkinVl@ya.ru>
2020-05-20 21:37:16 +0300

    Upfate

--- search_files.py
+++ search_files.py
@@ -1,4 +1,3 @@
-import os
 import os.path
 
 print(os.getcwd())
@@ -6,16 +5,12 @@ os.chdir("C:\\Users\\User\\Downloads\\")
 print(os.getcwd())
 
 list_of =[]
-for i in os.walk("."):
+for i in os.walk("main"):
     for file in i[2]:
         if file[-3:] == '.py':
-            list_of.append(i[0][2:])
+            list_of.append(i[0])
             break
 
-l = sorted(list_of)
-
-
 with open("C:\\Users\\User\\Downloads\\ans.txt", "w") as ans:
-    qwe = "\n".join(l)
-    ans.write(qwe)
+    ans.write("\n".join(sorted(list_of)))
 
